da3d17bbc73c1b5381fccc82f11bdd1767197277
walfredo <awalfredo@hotmail.com>
2022-01-16 23:03:24 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Form() {
+
+    return (
+<h1>Pagina de formulario</h1>
+
+    );
+}
+
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Listing() {
+
+    return (
+<h1>Pagina de listagem</h1>
+
+    );
+}
+
+
+export default Listing;
