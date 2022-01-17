52464486b42d180108252f9a6d05ae5bd165c655
walfredo <awalfredo@hotmail.com>
2022-01-16 23:52:17 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Pagination from 'components/Pagination'
 
 function Listing() {
 
     return (
-<h1>Pagina de listagem</h1>
+            <Pagination/>
 
     );
 }
