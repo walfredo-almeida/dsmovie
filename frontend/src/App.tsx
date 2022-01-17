bb4935d489a6edbaba4367e95b3bbeb07bf74960
walfredo <awalfredo@hotmail.com>
2022-01-16 21:33:11 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { ReactComponent as GithubIcon } from './assets/img/github.svg'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
- <h1>Pagina Principal normal</h1>
+   <Navbar/>
   );
 }
 
